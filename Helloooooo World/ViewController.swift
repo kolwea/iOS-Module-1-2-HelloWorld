fcd7cea3121901f4b6d119cba0545285e5253070
Kolbay <kolbe.weathington@gmail.com>
2019-01-31 18:16:39 -0600

    First commit

---
+++ Helloooooo World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Helloooooo World
+//
+//  Created by Weathington, Kolbe S. (MU-Student) on 1/31/19.
+//  Copyright © 2019 Weathington, Kolbe S. (MU-Student). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
